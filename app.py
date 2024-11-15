87a14979d49c53862a98abf9269efa78d49174ea
takahito fukutomi <takahito@test.com>
2024-11-15 09:31:58 +0900

    update app.py

--- app.py
+++ app.py
@@ -1,37 +1,51 @@
 from flask import Flask, request
 from azure.storage.blob import BlobServiceClient
+from imganalysis import analyze_image
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-### 環境変数読み込み
-# load_dotenv()
-
+# 環境変数読み込み
+load_dotenv()
+print("============================================1")
 app = Flask(__name__)
 
-# Azure Blob Storageの接続文字列とコンテナ名を設定
-connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-container_name = "uploadfiles"
-
-# BlobServiceClientを作成
-blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-container_client = blob_service_client.get_container_client(container_name)
-
-# コンテナが存在しない場合は作成
-if not container_client.exists():
-    container_client.create_container()
-
 # アップロード可能ファイル
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
     name, extension = os.path.splitext(filename)
     extension = extension.lstrip('.')
     return extension in ALLOWED_EXTENSIONS
 
+# BLOBに画像ファイルをアップロード
+def upload_file():
+    file = request.files['file']
+    if allowed_file(file.filename):
+        # Azure Blob Storageの接続文字列とコンテナ名を設定
+        connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+        container_name = os.getenv("AZURE_STORAGE_CONTAINER_NAME")
+
+        # BlobServiceClientを作成
+        blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+        container_client = blob_service_client.get_container_client(container_name)
+
+        # コンテナが存在しない場合は作成
+        if not container_client.exists():
+            container_client.create_container()
+        # 重複を避けるためにファイル名を生成する必要があるかも？
+        filename = file.filename
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=filename)
+        blob_client.upload_blob(file, overwrite=True)
+        return file.filename, 200
+    return "Invalid file type.", 400
+
 @app.route('/', methods=['POST'])
 def main():
+    # 画像をBLOBに保存
     result, status_code = upload_file()
     if status_code == 200:
-        return result, status_code
+        evaluation = analyze_image(result)
+        return evaluation
+        # return result, status_code
     else:
         return result, status_code
 
@@ -39,16 +53,6 @@ def main():
 def not_found():
     text = "oomaeai ver 0.01"
     return text
-
-def upload_file():
-    file = request.files['file']
-    if allowed_file(file.filename):
-        # 重複を避けるためにファイル名を生成する必要があるかも？
-        filename = file.filename
-        blob_client = blob_service_client.get_blob_client(container=container_name, blob=filename)
-        blob_client.upload_blob(file, overwrite=True)
-        return f"File {filename} uploaded to Blob Storage.", 200
-    return "Invalid file type.", 400
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ imganalysis.py
@@ -0,0 +1,64 @@
+import os
+from openai import AzureOpenAI
+from azure.storage.blob import BlobServiceClient, generate_container_sas, ContainerSasPermissions
+from datetime import datetime, timedelta, timezone
+
+from dotenv import load_dotenv # (LOCAL)
+
+def analyze_image(img_name):
+    # 環境変数読み込み(LOCAL)
+    load_dotenv()
+
+    # AzureOpenAIのインスタンスを生成　
+    client = AzureOpenAI(
+        api_version=os.getenv("OPENAI_API_VERSION"),
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+    )
+    model_name = "oomaeai-gpt-4o"
+
+    # Azure Storageの接続文字列
+    account_name = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
+    account_key = os.getenv("AZURE_STORAGE_ACCOUNT_KEY")
+    connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+    container_name = os.getenv("AZURE_STORAGE_CONTAINER_NAME")
+    container_url = os.getenv("AZURE_STORAGE_CONTAINER_URL")
+
+    # Blobサービスクライアントを作成
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+    # SASトークンを生成
+    sas_token = generate_container_sas(
+        account_name = account_name,
+        account_key = account_key,
+        container_name = container_name,
+        permission = ContainerSasPermissions(read=True),
+        expiry = datetime.now(timezone.utc) + timedelta(hours=1)  # 有効期限を1時間に設定（必要な時間に短縮するのが望ましい）
+    )
+    sas_para=f"?{sas_token}"
+
+    # 画像URL
+    blob_url = f"{container_url}/" + img_name + sas_para
+    img_url = blob_url
+
+    # LLMタスク
+    prompt = "URLの画像を見て、簡潔に説明してください。"
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": img_url
+                        },
+                    },
+                ],
+            }
+        ],
+        max_tokens=1200,
+    )
+    return response.choices[0].message.content 
